49e317c2bf2ed4affcde3af4466636082f127c00
era <era.user.ivan@gmail.com>
2022-05-30 09:44:30 +0300

    Init

---
+++ craco.config.js
@@ -0,0 +1,22 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: {
+                            '@primary-color': '#1890ff',
+                            '@border-radius-base': '8px',
+                            '@arrow-border-radius': '8px',
+                            '@typography-title-font-weight': '350',
+                        },
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    ],
+};
