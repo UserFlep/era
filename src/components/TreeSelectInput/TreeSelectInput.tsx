4ee425b0b01ae18b107b99df7b66dc813ddf68c6
era <era.user.ivan@gmail.com>
2022-07-04 18:48:03 +0300

    Облегчена парсировка данных с сервера в treeData, добавлены кнопки в каждый treeNode

--- src/components/TreeSelectInput/TreeSelectInput.tsx
+++ src/components/TreeSelectInput/TreeSelectInput.tsx
@@ -1,57 +1,69 @@
-import {TreeSelect} from 'antd';
+import {Button, TreeSelect} from 'antd';
 import React, {useEffect} from 'react';
-import { getTreeDataFromResponse } from '../../pages/SearchPage/utils';
+import classes from "./tree-select-input.module.less"
+import { PlusOutlined, DeleteOutlined} from '@ant-design/icons';
+import type { DefaultOptionType } from 'antd/es/select'
 import {useQuery} from "@apollo/client";
 import {GET_TAGS} from "../../requests/tag/Query";
 import {useMst} from "../../context";
-import {IOption} from "../../types/option";
 import { observer } from 'mobx-react-lite';
 
-const TreeSelectInput = observer(() => {
+interface INodeProps {
+    title: string
+}
+const TreeNodeContent: React.FC<INodeProps> = ({title})=>{
+    const mouseEnterHandler = ()=>{
+
+    }
+    const mouseLeaveHandler = ()=>{
+
+    }
+    return (
+        <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}} onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
+            <span>{title}</span>
+            <div>
+                <Button type="text" size={"small"} icon={<PlusOutlined style={{color: "#8c8c8c"}}/>}></Button>
+                <Button type="text" size={"small"} icon={<DeleteOutlined/>} danger></Button>
+            </div>
+        </div>
+    )
+}
+
+interface IProps {
+    maxTagCount?: number | 'responsive'
+}
+const TreeSelectInput:React.FC<IProps> = observer(({maxTagCount="responsive"}) => {
     const {tagStore} = useMst()
     const {loading, error, data } = useQuery(GET_TAGS);
-    const [treeData, setTreeData] = React.useState<IOption[]>();
-    // const [value, setValue] = React.useState<string[]>();
+    const [treeData, setTreeData] = React.useState<DefaultOptionType[]>();
 
     useEffect(()=>{
         if(data) {
-            setTreeData(getTreeDataFromResponse(data.tags))
+            const newTreeData = data.tags?.map((el:any)=>({id: el.id, pId: el.parentId, value: el.id, title: <TreeNodeContent title={el.name}/>}))
+            setTreeData(newTreeData)
         }
         console.log("useEffect--->Data fetching")
     },[data])
 
     const onChange = (newValue: string[]) => {
-        //setValue(newValue)
         tagStore.setSelectedList(newValue);
     };
 
-    // Если и делать, нужно прописать onClose
-    // const tagRender = (props:any) => {
-    //     const {label} = props;
-    //     return (
-    //         <Tag color={blue.primary} closable={true} style={{ marginRight: 3 }}>
-    //             {label}
-    //         </Tag>
-    //     );
-    // }
-
     return (
         <TreeSelect
-            // status={error ? "warning" : undefined}
-            // switcherIcon={<span>switcherIcon</span>}
+            status={error ? "warning" : undefined}
+            className={classes.treeSelect}
             showSearch
-            style={{width: '100%'}}
+            showCheckedStrategy={"SHOW_ALL"}
             value={tagStore.selectedItems}
-            //value={value}
-            dropdownStyle={{maxHeight: 500, overflow: 'auto'}}
             placeholder={loading ? "Получение данных..." : error ? "Ошибка получения данных!" : "Поиск по ключевым словам"}
-            fieldNames={{label: "title", value: "key", children: "children"}}
+            treeDataSimpleMode
             treeData={treeData}
             filterTreeNode
             treeNodeFilterProp="title"
+            maxTagCount={maxTagCount}
             allowClear
             multiple
-            // tagRender={tagRender}
             onChange={onChange}
         />
     );
