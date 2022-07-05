8ed06aa06fcfd119c9330d35cdba8c70df11b989
era <era.user.ivan@gmail.com>
2022-07-05 12:50:02 +0300

    добавлено модальное окно, работает плохо

--- src/components/TreeSelectInput/TreeNodeContent.tsx
+++ src/components/TreeSelectInput/TreeNodeContent.tsx
@@ -1,33 +1,61 @@
 import React from 'react';
 import classes from "./tree-select-input.module.less"
-import {Button, Tooltip} from "antd";
+import {Button, Input, Modal, Popconfirm, Popover, Tooltip} from "antd";
 import {DeleteOutlined, PlusOutlined} from "@ant-design/icons";
 
 interface IProps {
     title: string
 }
 const TreeNodeContent:React.FC<IProps> = ({title}) => {
-    const [visible, setVisible] = React.useState<boolean>(false)
+    const [buttonsVisible, setButtonsVisible] = React.useState<boolean>(false)
+    const [modalVisible, setModalVisible] = React.useState<boolean>(false)
+
     const mouseEnterHandler = ()=>{
-        setVisible(true)
+        setButtonsVisible(true)
     }
     const mouseLeaveHandler = ()=>{
-        setVisible(false)
+        setButtonsVisible(false)
     }
+
+    const addButtonHandler = (event:any)=>{
+        event.preventDefault();
+        event.stopPropagation();
+        setModalVisible(true);
+    }
+    const removeButtonHandler = ()=>{
+        //Запрос к бд на удаление тега по id
+    }
+
+    const modalOkHandler = () => {
+        setModalVisible(false);
+    };
+
+    const modalCancelHandler = () => {
+        setModalVisible(false);
+    };
+
     return (
+        <>
+        <Modal title="Добавление тега" visible={modalVisible} onOk={modalOkHandler} onCancel={modalCancelHandler} zIndex={1500}>
+            <Input placeholder="Тег"></Input>
+        </Modal>
         <div style={{display: "flex", justifyContent: "space-between", alignItems: "center"}} onMouseEnter={mouseEnterHandler} onMouseLeave={mouseLeaveHandler}>
             <span>{title}</span>
-            { visible &&
+            {buttonsVisible &&
                 <div className={classes.btnContainer}>
+
                     <Tooltip title="Добавить дочерний элемент">
-                        <Button type="text" size={"small"} icon={<PlusOutlined className={classes.addBtn}/>}></Button>
+                        <Button type="text" size={"small"} icon={<PlusOutlined className={classes.addBtn} onClick={e=>addButtonHandler(e)}/>}></Button>
                     </Tooltip>
+
                     <Tooltip title="Удалить">
-                        <Button type="text" size={"small"} icon={<DeleteOutlined className={classes.removeBtn}/>}></Button>
+                        <Button type="text" size={"small"} icon={<DeleteOutlined className={classes.removeBtn} onClick={removeButtonHandler}/>}></Button>
                     </Tooltip>
+
                 </div>
             }
         </div>
+        </>
     )
 };
 
